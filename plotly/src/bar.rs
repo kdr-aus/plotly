ae0153437015bc7a81f59d35b118842ac45e06e8
Kurt Lawrence <kurtlawrence92@gmail.com>
2020-07-10 13:55:39 +1000

    Rework Bar interface to place less restrictions on how Bar is used

--- plotly/src/bar.rs
+++ plotly/src/bar.rs
@@ -10,11 +10,7 @@ use serde::Serialize;
 use crate::private;
 
 #[derive(Serialize, Debug, Default)]
-pub struct Bar<X, Y>
-where
-    X: Serialize + Default,
-    Y: Serialize + Default,
-{
+pub struct Bar<X, Y> {
     x: Vec<X>,
     y: Vec<Y>,
     r#type: PlotType,
@@ -84,12 +80,12 @@ where
     y_calendar: Option<Calendar>,
 }
 
-impl<X, Y> Bar<X, Y>
-where
-    X: Serialize + Default,
-    Y: Serialize + Default,
-{
-    pub fn new(x: Vec<X>, y: Vec<Y>) -> Box<Bar<X, Y>> {
+impl<X, Y> Bar<X, Y> {
+    pub fn new(x: Vec<X>, y: Vec<Y>) -> Box<Self>
+    where
+        X: Serialize + Default,
+        Y: Serialize + Default,
+    {
         Box::new(Bar {
             x,
             y,
@@ -98,206 +94,206 @@ where
         })
     }
 
-    pub fn name(mut self, name: &str) -> Box<Bar<X, Y>> {
+    pub fn name(&mut self, name: &str) -> &mut Self {
         self.name = Some(name.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn visible(mut self, visible: bool) -> Box<Bar<X, Y>> {
+    pub fn visible(&mut self, visible: bool) -> &mut Self {
         self.visible = Some(visible);
-        Box::new(self)
+        self
     }
 
-    pub fn show_legend(mut self, show_legend: bool) -> Box<Bar<X, Y>> {
+    pub fn show_legend(&mut self, show_legend: bool) -> &mut Self {
         self.show_legend = Some(show_legend);
-        Box::new(self)
+        self
     }
 
-    pub fn legend_group(mut self, legend_group: &str) -> Box<Bar<X, Y>> {
+    pub fn legend_group(&mut self, legend_group: &str) -> &mut Self {
         self.legend_group = Some(legend_group.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn opacity(mut self, opacity: f64) -> Box<Bar<X, Y>> {
+    pub fn opacity(&mut self, opacity: f64) -> &mut Self {
         self.opacity = Some(opacity);
-        Box::new(self)
+        self
     }
 
-    pub fn ids<S: AsRef<str>>(mut self, ids: Vec<S>) -> Box<Bar<X, Y>> {
+    pub fn ids<S: AsRef<str>>(&mut self, ids: Vec<S>) -> &mut Self {
         let ids = private::owned_string_vector(ids);
         self.ids = Some(ids);
-        Box::new(self)
+        self
     }
 
-    pub fn width(mut self, width: usize) -> Box<Bar<X, Y>> {
+    pub fn width(&mut self, width: usize) -> &mut Self {
         self.width = Some(width);
-        Box::new(self)
+        self
     }
 
-    pub fn offset(mut self, offset: usize) -> Box<Bar<X, Y>> {
+    pub fn offset(&mut self, offset: usize) -> &mut Self {
         self.offset = Some(Dim::Scalar(offset));
-        Box::new(self)
+        self
     }
 
-    pub fn offset_array(mut self, offset: Vec<usize>) -> Box<Bar<X, Y>> {
+    pub fn offset_array(&mut self, offset: Vec<usize>) -> &mut Self {
         self.offset = Some(Dim::Vector(offset));
-        Box::new(self)
+        self
     }
 
-    pub fn text(mut self, text: &str) -> Box<Bar<X, Y>> {
+    pub fn text(&mut self, text: &str) -> &mut Self {
         self.text = Some(Dim::Scalar(text.to_owned()));
-        Box::new(self)
+        self
     }
 
-    pub fn text_array<S: AsRef<str>>(mut self, text: Vec<S>) -> Box<Bar<X, Y>> {
+    pub fn text_array<S: AsRef<str>>(&mut self, text: Vec<S>) -> &mut Self {
         let text = private::owned_string_vector(text);
         self.text = Some(Dim::Vector(text));
-        Box::new(self)
+        self
     }
 
-    pub fn text_position(mut self, text_position: TextPosition) -> Box<Bar<X, Y>> {
+    pub fn text_position(&mut self, text_position: TextPosition) -> &mut Self {
         self.text_position = Some(Dim::Scalar(text_position));
-        Box::new(self)
+        self
     }
 
-    pub fn text_position_array(mut self, text_position: Vec<TextPosition>) -> Box<Bar<X, Y>> {
+    pub fn text_position_array(&mut self, text_position: Vec<TextPosition>) -> &mut Self {
         self.text_position = Some(Dim::Vector(text_position));
-        Box::new(self)
+        self
     }
 
-    pub fn text_template(mut self, text_template: &str) -> Box<Bar<X, Y>> {
+    pub fn text_template(&mut self, text_template: &str) -> &mut Self {
         self.text_template = Some(Dim::Scalar(text_template.to_owned()));
-        Box::new(self)
+        self
     }
 
-    pub fn text_template_array<S: AsRef<str>>(mut self, text_template: Vec<S>) -> Box<Bar<X, Y>> {
+    pub fn text_template_array<S: AsRef<str>>(&mut self, text_template: Vec<S>) -> &mut Self {
         let text_template = private::owned_string_vector(text_template);
         self.text_template = Some(Dim::Vector(text_template));
-        Box::new(self)
+        self
     }
 
-    pub fn hover_text(mut self, hover_text: &str) -> Box<Bar<X, Y>> {
+    pub fn hover_text(&mut self, hover_text: &str) -> &mut Self {
         self.hover_text = Some(Dim::Scalar(hover_text.to_owned()));
-        Box::new(self)
+        self
     }
 
-    pub fn hover_text_array<S: AsRef<str>>(mut self, hover_text: Vec<S>) -> Box<Bar<X, Y>> {
+    pub fn hover_text_array<S: AsRef<str>>(&mut self, hover_text: Vec<S>) -> &mut Self {
         let hover_text = private::owned_string_vector(hover_text);
         self.hover_text = Some(Dim::Vector(hover_text));
-        Box::new(self)
+        self
     }
 
-    pub fn hover_info(mut self, hover_info: HoverInfo) -> Box<Bar<X, Y>> {
+    pub fn hover_info(&mut self, hover_info: HoverInfo) -> &mut Self {
         self.hover_info = Some(hover_info);
-        Box::new(self)
+        self
     }
 
-    pub fn hover_template(mut self, hover_template: &str) -> Box<Bar<X, Y>> {
+    pub fn hover_template(&mut self, hover_template: &str) -> &mut Self {
         self.hover_template = Some(Dim::Scalar(hover_template.to_owned()));
-        Box::new(self)
+        self
     }
 
-    pub fn x_axis(mut self, axis: &str) -> Box<Bar<X, Y>> {
+    pub fn x_axis(&mut self, axis: &str) -> &mut Self {
         self.x_axis = Some(axis.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn y_axis(mut self, axis: &str) -> Box<Bar<X, Y>> {
+    pub fn y_axis(&mut self, axis: &str) -> &mut Self {
         self.y_axis = Some(axis.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn hover_template_array<S: AsRef<str>>(mut self, hover_template: Vec<S>) -> Box<Bar<X, Y>> {
+    pub fn hover_template_array<S: AsRef<str>>(&mut self, hover_template: Vec<S>) -> &mut Self {
         let hover_template = private::owned_string_vector(hover_template);
         self.hover_template = Some(Dim::Vector(hover_template));
-        Box::new(self)
+        self
     }
 
-    pub fn orientation(mut self, orientation: Orientation) -> Box<Bar<X, Y>> {
+    pub fn orientation(&mut self, orientation: Orientation) -> &mut Self {
         self.orientation = Some(orientation);
-        Box::new(self)
+        self
     }
 
-    pub fn alignment_group(mut self, alignment_group: &str) -> Box<Bar<X, Y>> {
+    pub fn alignment_group(&mut self, alignment_group: &str) -> &mut Self {
         self.alignment_group = Some(alignment_group.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn offset_group(mut self, offset_group: &str) -> Box<Bar<X, Y>> {
+    pub fn offset_group(&mut self, offset_group: &str) -> &mut Self {
         self.offset_group = Some(offset_group.to_owned());
-        Box::new(self)
+        self
     }
 
-    pub fn marker(mut self, marker: Marker) -> Box<Bar<X, Y>> {
+    pub fn marker(&mut self, marker: Marker) -> &mut Self {
         self.marker = Some(marker);
-        Box::new(self)
+        self
     }
 
-    pub fn text_angle(mut self, text_angle: f64) -> Box<Bar<X, Y>> {
+    pub fn text_angle(&mut self, text_angle: f64) -> &mut Self {
         self.text_angle = Some(text_angle);
-        Box::new(self)
+        self
     }
 
-    pub fn text_font(mut self, text_font: Font) -> Box<Bar<X, Y>> {
+    pub fn text_font(&mut self, text_font: Font) -> &mut Self {
         self.text_font = Some(text_font);
-        Box::new(self)
+        self
     }
 
-    pub fn error_x(mut self, error_x: ErrorData) -> Box<Bar<X, Y>> {
+    pub fn error_x(&mut self, error_x: ErrorData) -> &mut Self {
         self.error_x = Some(error_x);
-        Box::new(self)
+        self
     }
 
-    pub fn error_y(mut self, error_y: ErrorData) -> Box<Bar<X, Y>> {
+    pub fn error_y(&mut self, error_y: ErrorData) -> &mut Self {
         self.error_y = Some(error_y);
-        Box::new(self)
+        self
     }
 
-    pub fn clip_on_axis(mut self, clip_on_axis: bool) -> Box<Bar<X, Y>> {
+    pub fn clip_on_axis(&mut self, clip_on_axis: bool) -> &mut Self {
         self.clip_on_axis = Some(clip_on_axis);
-        Box::new(self)
+        self
     }
 
-    pub fn constrain_text(mut self, constrain_text: ConstrainText) -> Box<Bar<X, Y>> {
+    pub fn constrain_text(&mut self, constrain_text: ConstrainText) -> &mut Self {
         self.constrain_text = Some(constrain_text);
-        Box::new(self)
+        self
     }
 
-    pub fn hover_label(mut self, hover_label: Label) -> Box<Bar<X, Y>> {
+    pub fn hover_label(&mut self, hover_label: Label) -> &mut Self {
         self.hover_label = Some(hover_label);
-        Box::new(self)
+        self
     }
 
-    pub fn inside_text_anchor(mut self, inside_text_anchor: TextAnchor) -> Box<Bar<X, Y>> {
+    pub fn inside_text_anchor(&mut self, inside_text_anchor: TextAnchor) -> &mut Self {
         self.inside_text_anchor = Some(inside_text_anchor);
-        Box::new(self)
+        self
     }
 
-    pub fn inside_text_font(mut self, inside_text_font: Font) -> Box<Bar<X, Y>> {
+    pub fn inside_text_font(&mut self, inside_text_font: Font) -> &mut Self {
         self.inside_text_font = Some(inside_text_font);
-        Box::new(self)
+        self
     }
 
-    pub fn outside_text_font(mut self, outside_text_font: Font) -> Box<Bar<X, Y>> {
+    pub fn outside_text_font(&mut self, outside_text_font: Font) -> &mut Self {
         self.outside_text_font = Some(outside_text_font);
-        Box::new(self)
+        self
     }
 
-    pub fn x_calendar(mut self, x_calendar: Calendar) -> Box<Bar<X, Y>> {
+    pub fn x_calendar(&mut self, x_calendar: Calendar) -> &mut Self {
         self.x_calendar = Some(x_calendar);
-        Box::new(self)
+        self
     }
 
-    pub fn y_calendar(mut self, y_calendar: Calendar) -> Box<Bar<X, Y>> {
+    pub fn y_calendar(&mut self, y_calendar: Calendar) -> &mut Self {
         self.y_calendar = Some(y_calendar);
-        Box::new(self)
+        self
     }
 }
 
 impl<X, Y> Trace for Bar<X, Y>
 where
-    X: Serialize + Default,
-    Y: Serialize + Default,
+    X: Serialize,
+    Y: Serialize,
 {
     fn serialize(&self) -> String {
         serde_json::to_string(&self).unwrap()
